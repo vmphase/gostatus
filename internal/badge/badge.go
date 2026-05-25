632306cd2623644ede30fc52466a353c04958ac6
vmphase <vmphasee@gmail.com>
2026-05-25 12:23:28 +0300

    feat(badge): flat-square style support

--- internal/badge/badge.go
+++ internal/badge/badge.go
@@ -13,14 +13,19 @@ type Style string
 
 const (
 	StyleFlat        Style = "flat"
+	StyleFlatSquare  Style = "flat-square"
 	StyleForTheBadge Style = "for-the-badge"
 )
 
 func ParseStyle(s string) Style {
-	if s == "for-the-badge" {
+	switch s {
+	case "flat-square":
+		return StyleFlatSquare
+	case "for-the-badge":
 		return StyleForTheBadge
+	default:
+		return StyleFlat
 	}
-	return StyleFlat
 }
 
 var StatusColors = map[string]string{
