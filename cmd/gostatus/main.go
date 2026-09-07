8e2c5cac5d864163b63a379b1ad2102d53283201
vmphase <vmphase.dev@gmail.com>
2026-09-07 22:38:04 +0300

    feat: add graceful shutdown

--- cmd/gostatus/main.go
+++ cmd/gostatus/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"gostatus/internal/config"
@@ -40,7 +43,25 @@ func main() {
 		IdleTimeout:       30 * time.Second,
 	}
 	log.Printf("Listening on :%s", cfg.Port)
-	log.Fatal(srv.ListenAndServe())
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	log.Println("Shutting down...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Graceful shutdown failed: %v", err)
+	}
 }
 
 func checkHealth(port string) error {
